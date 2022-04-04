04b5e9cfd4218dfd1bd653e49af55612aee28566
Estefano_Yu <2322740290@qq.com>
2022-04-04 18:00:02 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,17 +8,37 @@ with open('LICENSE') as f:
   license = f.read()
 
 setup(
-    name='basenji',
-    version='0.1',
-    description='Sequential regulatory activity machine learning',
-    long_description=readme,
-    author='David Kelley',
-    author_email='drk@calicolabs.com',
-    url='https://github.com/calico/basenji',
-    license=license,
+    name="basenji",
+    # TODO: Consider using https://github.com/python-versioneer/python-versioneer to
+    # get version information from git.
+    version="0.1.0-dev0",
+    description="A sample Python project",
+    # author='A. Random Developer',
+    # author_email='author@example.com',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
     packages=find_packages(exclude=('tests', 'docs')),
+    python_requires=">=3.6, <4",
+    # Do not install tensorflow here, because might want to use tensorflow or
+    # tensorflow-cpu.
     install_requires=[
         l.strip() for l in
         Path('requirements.txt').read_text('utf-8').splitlines()
     ]
+    extras_require={
+        "dev": [
+            "black",  # styler
+            "flake8",  # linter
+        ],
+    },
 )
